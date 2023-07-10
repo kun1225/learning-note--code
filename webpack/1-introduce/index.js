38f80e1226be9525ef53fc6233359c6b0d67221a
thisWeb1225 <shan881225@gmail.com>
2023-07-10 20:44:30 +0800

    :memo: Docs: add webpack 2-basic.md

--- webpack/1-introduce/index.js
+++ webpack/1-introduce/index.js
@@ -1,4 +1,11 @@
 // index.js  
 const moment = require('moment');
-console.log("Hello from JavaScript!");  
+import imgSrc from './assets/webpack.jpg';
+import './index.scss';
+
+console.log("Hello from JavaScript!!");  
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+const img = document.createElement('img');
+img.src = imgSrc
+document.querySelector('body').appendChild(img);
--- webpack/1-introduce/webpack.config.js
+++ webpack/1-introduce/webpack.config.js
@@ -1,9 +1,34 @@
 // webpack.config.js  
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {  
   mode: 'development',  
   entry: './index.js',  
   output: {  
-    filename: 'main.js',  
-    publicPath: 'dist'  
-  }  
+    filename: 'main.[contenthash].js',  
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
 };
